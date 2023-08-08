1b628a5260cd0c9af6ad92ed7b28eb2100a4da3a
Vlad <dkfl566@yandex.ru>
2023-08-08 11:53:20 +0500

    Homework

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+# from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Успешно')
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top_sellers.html')
+    
--- project/project/settings.py
+++ project/project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    Path.joinpath(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
