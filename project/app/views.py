0a3d97efb0a98f55d8037f075caa9178aa8d393e
Vlad <dkfl566@yandex.ru>
2023-08-18 16:07:50 +0500

    Я реализовал палидром с помощьюж шага -1

--- project/app/views.py
+++ project/app/views.py
@@ -5,5 +5,5 @@ def index(request):
     return render(request, 'index.html')
 
 def top_sellers(request):
-    return render(request, 'top_sellers.html')
+    return render(request, 'top-sellers.html')
     
