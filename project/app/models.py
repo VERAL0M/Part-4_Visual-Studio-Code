b3fdbc7afe3cfd7ea52926fbc9184e2009ea7ccb
Vlad <dkfl566@yandex.ru>
2023-08-21 11:03:53 +0500

    Задание №4 к 7 уроку

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('заголовок', max_length=128) 
+    description = models.TextField('Описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если уместен торг')
